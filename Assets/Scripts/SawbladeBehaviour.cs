4235acc97748c88e2d61bf106bf44423de5d8938
ViniciusVsV <viniciusvsv193@gmail.com>
2024-10-31 14:28:57 -0300

    tirando comentarios kk

--- Assets/Scripts/SawbladeBehaviour.cs
+++ Assets/Scripts/SawbladeBehaviour.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SawbladeBehaviour : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float rotationSpeed; // Velocidade de rotação da serra
+    [SerializeField] private float rotationSpeed;
     [SerializeField] private Transform point1;
     [SerializeField] private Transform point2;
     private Transform targetPoint;
@@ -17,20 +17,16 @@ public class SawbladeBehaviour : MonoBehaviour
 
     void Update()
     {
-        // Move a serra entre os pontos
         transform.position = Vector2.MoveTowards(transform.position, targetPoint.position, moveSpeed * Time.deltaTime);
 
-        // Verifica o ponto de destino e altera o alvo
         if(transform.position == point1.position)
             targetPoint = point2;
         else if(transform.position == point2.position)
             targetPoint = point1;
 
-        // Gira a serra
         RotateSawblade();
     }
 
-    // Função para girar o sprite da serra
     void RotateSawblade()
     {
         transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
